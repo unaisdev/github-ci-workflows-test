83f9be57f4d65497311cbb4552f27dd4d69eeeff
unaisdev <unaisdev@gmail.com>
2024-08-01 23:23:56 +0200

    puedeser?o ahora?

--- create-branch.sh
+++ create-branch.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+#Fail on first error
+set -e
+
 echo "hello world" > "document.md"
+
+git add document.md
+git commit -m "document.md"
+
+#Check if branch name is provided
+if [ -z "$BRANCH_NAME" ]; then
+  echo "Error: BRANCH_NAME is not set."
+  exit 1
+fi
+
+#Create a new branch
+git checkout -b $BRANCH_NAME
+
